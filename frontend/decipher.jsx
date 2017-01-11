03ff14b64e19049202280da33e6b01ffba1b1605
Andrew Lim <andyl319@yahoo.com>
2017-01-10 16:57:35 -0800

    finish user auth back end and start implementation of user auth front end

--- frontend/decipher.jsx
+++ frontend/decipher.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login, logout} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Decipher</h1>, root);
+    window.login = login;
+    window.logout = logout;
 });
