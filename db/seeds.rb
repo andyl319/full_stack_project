72c25cd92107f1521bccc451055b9e022aa832e4
Andrew Lim <andyl319@yahoo.com>
2017-01-16 03:21:00 -0800

    track show page style

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,98 +21,98 @@ telegraphAve = Track.create(
   album: "Because the Internet",
   description: "Telegraph Ave is the 7'th track on Childish Gambino's
     2'nd studio album title Because the Internet",
-  lyrics: "[Verse 1: Lloyd]
-    I was making Japanese
-    And she's watching DVDs
-    In Oakland, in Oakland
-    Now I'm driving up the 5
-    And she waits till I arrive
-    In Oakland, in Oakland
-    Everything, that I wanted
-    Only gotta drive for the moment
-    If you tell me 'turn around,' then I'm on it
-    For the moment, but you know me
-    You know how I get when I'm lonely
-    And I think about you and the moments
-    But everything you do is so Oakland, so Oakland
+  lyrics: "[Verse 1: Lloyd]\n
+    I was making Japanese\n
+    And she's watching DVDs\n
+    In Oakland, in Oakland\n
+    Now I'm driving up the 5\n
+    And she waits till I arrive\n
+    In Oakland, in Oakland\n
+    Everything, that I wanted\n
+    Only gotta drive for the moment\n
+    If you tell me 'turn around,' then I'm on it\n
+    For the moment, but you know me\n
+    You know how I get when I'm lonely\n
+    And I think about you and the moments\n
+    But everything you do is so Oakland, so Oakland\n\n
 
-    [Hook 1: Lloyd]
-    Foot on the gas
-    I'm just trying to pass
-    All the red lights
-    And the stop signs
-    I'm ready to go
-    Before I get to The Bay
-    Babe, that's a problem
-    Because I'm way too scared to call
-    And you might get me to stay, and oh
+    [Hook 1: Lloyd]\n
+    Foot on the gas\n
+    I'm just trying to pass\n
+    All the red lights\n
+    And the stop signs\n
+    I'm ready to go\n
+    Before I get to The Bay\n
+    Babe, that's a problem\n
+    Because I'm way too scared to call\n
+    And you might get me to stay, and oh\n\n
 
-    [Post-Hook: Childish Gambino and Lloyd]
-    I don't really mind the drive
-    But I think I'd rather die
-    In Oakland, in Oakland
-    With my hands on two and ten
-    So I guess it all depends
-    On Oakland, on Oakland
-    And I'm nervous, truth be told
-    I never saw me growing old
-    In Oakland, in Oakland
-    And if I married you tonight
-    It would probably start a riot
-    In Oakland, in Oakland
+    [Post-Hook: Childish Gambino and Lloyd]\n
+    I don't really mind the drive\n
+    But I think I'd rather die\n
+    In Oakland, in Oakland\n
+    With my hands on two and ten\n
+    So I guess it all depends\n
+    On Oakland, on Oakland\n
+    And I'm nervous, truth be told\n
+    I never saw me growing old\n
+    In Oakland, in Oakland\n
+    And if I married you tonight\n
+    It would probably start a riot\n
+    In Oakland, in Oakland\n\n
 
-    [Verse 2: Lloyd]
-    Everything, that I needed
-    Now that I got you in your feelings
-    Everything you won't say, you tweet it
-    And a nigga don’t like that shit at all
-    So treated
-    Can we just roll with the feeling?
-    Can we just roll for a minute?
-    Wait a minute
+    [Verse 2: Lloyd]\n
+    Everything, that I needed\n
+    Now that I got you in your feelings\n
+    Everything you won't say, you tweet it\n
+    And a nigga don’t like that shit at all\n
+    So treated\n
+    Can we just roll with the feeling?\n
+    Can we just roll for a minute?\n
+    Wait a minute\n\n
 
-    [Hook 2: Lloyd]
-    Foot on the gas
-    I'm just trying to pass
-    All the red lights
-    And the stop signs
-    I'm ready to go
-    But I'm really not ready, girl
-    That's a problem
-    Cause I'm way too scared to fall
-    And I know you choose to stay, and oh
+    [Hook 2: Lloyd]\n
+    Foot on the gas\n
+    I'm just trying to pass\n
+    All the red lights\n
+    And the stop signs\n
+    I'm ready to go\n
+    But I'm really not ready, girl\n
+    That's a problem\n
+    Cause I'm way too scared to fall\n
+    And I know you choose to stay, and oh\n\n
 
-    [Verse 3: Childish Gambino]
-    All the girlfriends saying 'here we go again'
-    Rich kid but he act like a gentleman
-    Last one didn't end like it should've been
-    Two dates and he still wanna get it in
-    And they're saying it's because of the Internet
-    Try her once and it's on to the next chick
-    X-O the O face on your exes...right?
-    And we can do the same thing if you wanna have at it
-    When your thoughts can't breathe and you thinking asthmatic
-    And you wanna be a mom and I wasn't mad at it
-    I was thinking 'bout me, I'd be really bad at it
-    Cause I'm thinking 'bout me, weeks in Dubai
-    Fourth of July, house in Kauai, yeah we can try
-    So let's try
+    [Verse 3: Childish Gambino]\n
+    All the girlfriends saying 'here we go again'\n
+    Rich kid but he act like a gentleman\n
+    Last one didn't end like it should've been\n
+    Two dates and he still wanna get it in\n
+    And they're saying it's because of the Internet\n
+    Try her once and it's on to the next chick\n
+    X-O the O face on your exes...right?\n
+    And we can do the same thing if you wanna have at it\n
+    When your thoughts can't breathe and you thinking asthmatic\n
+    And you wanna be a mom and I wasn't mad at it\n
+    I was thinking 'bout me, I'd be really bad at it\n
+    Cause I'm thinking 'bout me, weeks in Dubai\n
+    Fourth of July, house in Kauai, yeah we can try\n
+    So let's try\n\n
 
-    [Outro: Lloyd]
-    Whoa
-    I took off my nine to five
-    But you still don't have the time to kiss me, just hit me
-    And if I left you all alone, would you still pick up the phone?
-    It's iffy, you miss me
-    I got furniture to move
-    And we'll both be thirty soon
-    In Oakland, in Oakland
-    The only one I know is you, so the fuck I'm supposed to do?
-    In Oakland, in Oakland
-    In Oakland, in Oakland
-    In Oakland, in Oakland
-    In Oakland, in Oakland
-    In Oakland, in Oakland
+    [Outro: Lloyd]\n
+    Whoa\n
+    I took off my nine to five\n
+    But you still don't have the time to kiss me, just hit me\n
+    And if I left you all alone, would you still pick up the phone?\n
+    It's iffy, you miss me\n
+    I got furniture to move\n
+    And we'll both be thirty soon\n
+    In Oakland, in Oakland\n
+    The only one I know is you, so the fuck I'm supposed to do?\n
+    In Oakland, in Oakland\n
+    In Oakland, in Oakland\n
+    In Oakland, in Oakland\n
+    In Oakland, in Oakland\n
+    In Oakland, in Oakland\n
     In Oakland, in Oakland",
   cover_art: "https://images.genius.com/54176aed36cb6114bc46f0b70b8b02b3.1000x1000x1.jpg",
   youtube_url: "https://www.youtube.com/v/NPQCra8FEew"
@@ -161,6 +161,82 @@ We’ll become who we're meant because I know
 We’ll become who we're meant to be",
   cover_art: "https://images.genius.com/c19df9e79cbdf69a0c00fe8f06598a0a.960x960x1.jpg",
   youtube_url: "https://www.youtube.com/v/tAhT6kFWkAo"
+)
+
+all_of_me = Track.create(
+  user_id: 1,
+  title: "All of Me",
+  artist: "John Legend",
+  album: "Love in the Future",
+  description: "“All of Me” is the third single from and sixth
+  track on John Legend’s album",
+  lyrics: "[Verse 1]
+What would I do without your smart mouth?
+Drawing me in and you kicking me out
+You got my head spinning, no kidding, I can't pin you down
+What's going on in that beautiful mind?
+I'm on your magical mystery ride
+And I'm so dizzy, don't know what hit me, but I'll be all right
+
+[Pre-Hook]
+My head's under water but I’m breathing fine
+You're crazy and I'm out of my mind
+
+[Hook]
+Cause all of me loves all of you
+Love your curves and all your edges
+All your perfect imperfections
+Give your all to me
+I'll give my all to you
+You're my end and my beginning
+Even when I lose, I’m winning
+Cause I give you all of me
+And you give me all of you
+
+[Verse 2]
+How many times do I have to tell you?
+Even when you're crying, you're beautiful too
+The world is beating you down, I’m around
+Through every mood
+You're my downfall, you're my muse
+My worst distraction, my rhythm and blues
+I can't stop singing, it's ringing in my head for you
+
+[Pre-Hook]
+My head's under water but I’m breathing fine
+You're crazy and I'm out of my mind
+
+[Hook]
+Cause all of me loves all of you
+Love your curves and all your edges
+All your perfect imperfections
+Give your all to me
+I'll give my all to you
+You're my end and my beginning
+Even when I lose, I’m winning
+Cause I give you all of me
+And you give me all of you
+
+[Bridge]
+Cards on the table, we're both showing hearts
+Risking it all, though it’s hard
+
+[Hook]
+Cause all of me loves all of you
+Love your curves and all your edges
+All your perfect imperfections
+Give your all to me
+I'll give my all to you
+You're my end and my beginning
+Even when I lose, I’m winning
+Cause I give you all of me
+And you give me all of you
+
+[Outro]
+I give you all of me
+And you give me all of you",
+  cover_art: "http://images.genius.com/e95068be211e9b806bba3f57a14c7100.1000x994x1.jpg",
+  youtube_url: "https://www.youtube.com/watch?v=450p7goxZqg"
 )
 
 black_friday = Track.create(
