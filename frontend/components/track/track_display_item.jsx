27489f53bd638dd5a8bfc26991583ce9342d5994
Andrew Lim <andyl319@yahoo.com>
2017-03-15 22:18:28 -0500

    resize track display on home page

--- frontend/components/track/track_display_item.jsx
+++ frontend/components/track/track_display_item.jsx
@@ -5,7 +5,7 @@ const TrackDisplayItem = ({track, router}) => {
   return (
     <span className="track-display-item">
       <Link to={`/tracks/${track.id}`}>
-        <img src={track.cover_art} alt={track.title} className="cover-art" />
+        <img src={track.cover_art} alt={track.title} className="track-display-cover-art" />
         <ul className="track-display-detail">
           <li className="track-display-title">{track.title}</li>
           <li className="track-display-artist">{track.artist}</li>
