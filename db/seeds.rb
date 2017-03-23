50a01fb4d4236c0b14ef40d217063253991d01c9
Andrew Lim <andyl319@yahoo.com>
2017-03-23 18:19:50 -0500

    new screenshots

--- db/seeds.rb
+++ db/seeds.rb
@@ -238,7 +238,7 @@ I feel it coming, babe
 I feel it coming, babe
 I feel it coming, babe",
   cover_art: "http://images.genius.com/02d44f528e2f14794c9ac02eeb6268f3.1000x1000x1.jpg",
-  youtube_url: "https://player.vimeo.com/video/207968051"
+  youtube_url: "https://player.vimeo.com/video/204684364"
 )
 
 selena = Track.create(
